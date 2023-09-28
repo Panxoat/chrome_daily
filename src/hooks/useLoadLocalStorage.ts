101f001a9e151a24cd798c5f068ac685fc0a52fa
Panxoat <a20304645@gmail.com>
2023-09-28 23:18:00 +0900

    fix: build error

--- src/hooks/useLoadLocalStorage.ts
+++ src/hooks/useLoadLocalStorage.ts
@@ -1,10 +1,19 @@
 "use client";
+
+import { useEffect, useState } from "react";
+
 interface UseLocalStorageProps {
   key: string;
 }
 
 export const useLoadLocalStorage = <T>({ key }: UseLocalStorageProps) => {
-  const localStorageData = window.localStorage.getItem(key);
+  const [data, setData] = useState<T | null>(null);
 
-  return localStorageData ? (JSON.parse(localStorageData) as T) : null;
+  useEffect(() => {
+    const localStorageData = window.localStorage.getItem(key);
+
+    setData(localStorageData ? (JSON.parse(localStorageData) as T) : null);
+  }, []);
+
+  return data;
 };
